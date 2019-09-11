ff27e7e60fee0ba1f009a0c1620ebc018dd27482
Joe Huss <detain@interserver.net>
2019-09-11 16:31:21 -0400

    Initial commit adding all my current code

---
+++ photos-api.php
@@ -0,0 +1,40 @@
+<?php
+function getImageIdx($idx, $totalImages) {
+    $idx = $_SESSION['start'] + $idx;
+    if ($idx >= $totalImages)
+        $idx -= $totalImages;
+    return $idx;
+} 
+header('Content-type: application/json; charset=UTF-8');
+session_start();
+error_log($_SESSION['start']);
+$photos = [];
+$type = isset($_GET['type']) ? $_GET['type'] : 'gifs';
+if (!in_array($type, ['gifs', 'images', 'wallpapers']))
+    $type = 'gifs';
+$result = file_get_contents(__DIR__.'/'.$type.'.txt'); // generated with file * > gifs.txt
+preg_match_all('/^([^:]*):\s+.* (\d+)\s?x\s?(\d+)[^0-9].*$/msuU', $result, $matches);
+$pageLimit = 5;
+$pageIdx = isset($_GET['page']) ? intval($_GET['page']) : 1;
+$totalImages = count($matches[0]);
+$imageStart = ($pageIdx - 1) * $pageLimit; 
+$imageEnd = $pageIdx * $pageLimit;
+if ($imageEnd > $totalImages)
+    $imageEnd = $totalImages;
+if ($pageIdx == 1)
+    $_SESSION['start'] = rand(0, $totalImages - 1); 
+for ($idx = $imageStart; $idx < $imageEnd; $idx++) {
+    $realIdx = getImageIdx($idx, $totalImages);
+    $photos[] = [
+        'type' => $type,
+        'file' => [
+            'src' => $matches[1][$realIdx],
+            'name' => substr($matches[1][$realIdx], 0, -4),
+        ],
+        'size' => [
+            'width' => $matches[2][$realIdx],
+            'height' => $matches[3][$realIdx]
+        ] 
+    ]; 
+} 
+echo json_encode($photos);
---
+++ photos.js
@@ -0,0 +1,80 @@
+var gridType = 'packery'; // masonry packery or isotope
+var pageType = 'gifs'; // gifs images or wallpapers
+function pageScroll() {
+        window.scrollBy(0,4); // horizontal and vertical scroll increments
+        scrolldelay = setTimeout('pageScroll()',100); // scrolls every 100 milliseconds
+}
+if (gridType == 'masonry') {
+    var $grid = $(".grid").masonry({
+        itemSelector: ".photo-item",
+        columnWidth: ".grid__col-sizer",
+        gutter: ".grid__gutter-sizer",
+        percentPosition: false,
+        stagger: 30,
+        visibleStyle: { transform: "translateY(0)", opacity: 1 }, // nicer reveal transition
+        hiddenStyle: { transform: "translateY(100px)", opacity: 0 }
+    });
+    var gridInsance = $grid.data("masonry"); // get Masonry instance
+} else if (gridType == 'packery') {
+    var $grid = $(".grid").packery({
+        itemSelector: ".photo-item",
+        gutter: ".grid__gutter-sizer",
+        percentPosition: false,
+        stagger: 30,
+        visibleStyle: { transform: "translateY(0)", opacity: 1 }, // nicer reveal transition
+        hiddenStyle: { transform: "translateY(100px)", opacity: 0 }
+    });
+    var gridInsance = $grid.data("packery"); // get Packery instance
+} else if (gridType == 'isotope') {
+    var $grid = $(".grid").isotope({
+        layoutMode: 'fitRows', // masonry fitRows cellsByRow vertical packery masonryHorizontal fitColumns cellsByColumn horiz
+        itemSelector: ".photo-item",
+        gutter: ".grid__gutter-sizer",
+        percentPosition: false,
+        stagger: 30,
+        visibleStyle: { transform: "translateY(0)", opacity: 1 }, // nicer reveal transition
+        hiddenStyle: { transform: "translateY(100px)", opacity: 0 }
+    });
+    var gridInsance = $grid.data("isotope"); // get Packery instance
+}
+$grid.infiniteScroll.imagesLoaded = imagesLoaded;
+$grid.infiniteScroll({
+    path: function() {
+        return ("photos-api.php?type=" + pageType + "&page=" + this.pageIndex);
+    },
+    responseType: "text", // load response as flat text
+    outlayer: gridInsance,
+    status: ".page-load-status",
+    history: false
+});
+$grid.on("load.infiniteScroll", function(event, response) {
+    var data = JSON.parse(response); // parse response into JSON data
+    var itemsHTML = data.map(getItemHTML).join(""); // compile data into HTML
+    var $items = $(itemsHTML); // convert HTML string into elements
+    $items.imagesLoaded(function() { // append item elements
+        if (gridType == 'masonry') {
+            $grid.infiniteScroll("appendItems", $items).masonry("appended", $items);
+        } else if (gridType == 'packery') {
+            $grid.infiniteScroll("appendItems", $items).packery("appended", $items);
+        } else if (gridType == 'isotope') {
+            $grid.infiniteScroll("appendItems", $items).isotope("appended", $items);
+        }
+        if ($('.photo-item').length < 50)
+            $grid.infiniteScroll("loadNextPage"); // load initial pag
+    });
+});
+$grid.infiniteScroll("loadNextPage"); // load initial page
+var itemTemplateSrc = $("#photo-item-template").html();
+function getItemHTML(photo) {
+    return microTemplate(itemTemplateSrc, photo);
+}
+function microTemplate(src, data) { // micro templating, sort-of
+    return src.replace(/\{\{([\w\-_\.]+)\}\}/gi, function(match, key) { // replace {{tags}} in source
+        var value = data; // walk through objects to get value
+        key.split(".").forEach(function(part) {
+            value = value[part];
+        });
+        return value;
+    });
+}
+pageScroll();
