746b1d7ccc1dd3798309facb3097d80e27aeb570
Joe Huss <detain@interserver.net>
2019-09-20 17:31:56 -0400

    added loading the server.json file and pulling the live formats and types from that as well as adding displaying the counts

--- options.js
+++ options.js
@@ -1,16 +1,19 @@
-var formats = ["ALL", "JPG", "PNG", "GIF", "MPEG-4"];
-var cats = ["anime", "reddit_sub_ProgrammerHumor", "wallpapers"];
-for (var i = 0; i < formats.length; ++i) {
-  var format = formats[i];
-  var catId = "format" + format;
-  $("#formatsContainer").append('<input id="' + catId + '" name="format" type="radio"><label for="' + catId + '">' + format + "</label>");
-}
-for (var i = 0; i < cats.length; ++i) {
-  var cat = cats[i];
-  var catId = "toggle" + cat;
-  var catLabel = cat.replace("reddit_sub_", "r/");
-  $("#typesContainer").append('<input id="' + catId + '" name="type[]" type="checkbox"><label for="' + catId + '">' + catLabel + "</label>");
-}
+var server;
+jQuery.getJSON('server.json', {}, function (json) {
+    server = json;
+    $("#formatsContainer").append('<input id="formatALL" name="format" type="radio" value="ALL" checked><label for="formatALL">ALL</label>');
+    for (var i = 0; i < server.formats.length; ++i) {
+      var format = server.formats[i];
+      var catId = "format" + format;
+      $("#formatsContainer").append('<input id="' + catId + '" name="format" type="radio" value="' + format + '"><label for="' + catId + '">' + format + " (" + server.counts.formats[format] + ")</label>");
+    }
+    for (var i = 0; i < server.dirs.length; ++i) {
+      var cat = server.dirs[i];
+      var catId = "toggle" + cat;
+      var catLabel = cat.replace("reddit_sub_", "r/");
+      $("#typesContainer").append('<input id="' + catId + '" name="type[]" type="checkbox" value="' + cat + '"><label for="' + catId + '">' + catLabel + " (" + server.counts.dirs[cat] + ")</label>");
+    }
+});
 $("#typesContainer input").each(function() {
   if ($(this).prop("checked")) {
     var cat = $(this).prop("id").replace("toggle", "");
