d157310aee8cb7b1d4f8adb52cc40e968455913b
Joe Huss <detain@interserver.net>
2019-09-20 21:14:53 -0400

    updates adding in the options stuf into thep hotos page

--- photos.js
+++ photos.js
@@ -2,6 +2,29 @@ var formatType = 'ALL';
 var audioType = 'null';
 var gridType = 'packery'; // masonry packery or isotope
 var pageType = 'reddit_sub_ProgrammerHumor'; // gifs images or wallpapers
+var server;
+jQuery.getJSON('https://vault3.is.cc/consolo/neverending-eye-candy-gallery/server.json', {}, function (json) {
+    server = json;
+    $("#formatsContainer").append('<input id="formatALL" name="format" type="radio" value="ALL" checked><label for="formatALL">ALL</label>');
+    for (var i = 0; i < server.formats.length; ++i) {
+      var format = server.formats[i];
+      var catId = "format" + format;
+      $("#formatsContainer").append('<input id="' + catId + '" name="format" type="radio" value="' + format + '"><label for="' + catId + '">' + format + "<span style='float: right;'>" + server.counts.formats[format] + "</span></label>");
+    }
+    for (var i = 0; i < server.dirs.length; ++i) {
+      var cat = server.dirs[i];
+      var catId = "toggle" + cat;
+      var catLabel = cat.replace("reddit_sub_", "");
+      $("#typesContainer").append('<input id="' + catId + '" name="type[]" type="checkbox" value="' + cat + '"><label for="' + catId + '">' + catLabel + "<span style='float: right;'>" + server.counts.dirs[cat] + "</span></label>");
+    }
+});
+$("#typesContainer input").each(function() {
+  if ($(this).prop("checked")) {
+    var cat = $(this).prop("id").replace("toggle", "");
+  }
+});
+
+
 if (pageType == 'family') gridType = 'masonry';
 $('#photo-grid').addClass(gridType);
 function pageScroll() {
@@ -87,4 +110,4 @@ function microTemplate(src, data) { // micro templating, sort-of
         return value;
     });
 }
-pageScroll();
+//pageScroll();
